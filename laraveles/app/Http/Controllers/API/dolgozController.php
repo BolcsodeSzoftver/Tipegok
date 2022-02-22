c1571eafe44de3de0467182e8eed78ca061a6eaa
KNikooo <kissnikolettanett@gmail.com>
2022-02-22 19:40:50 +0100

    hibas rendezes

--- laraveles/app/Http/Controllers/API/dolgozController.php
+++ laraveles/app/Http/Controllers/API/dolgozController.php
@@ -155,9 +155,32 @@ class dolgozController extends Controller
         return $tasks->get();
     } */
 
+    public $search='';
     public function allapot(){
         $alkalmazott= alkalmazott::all();
         $alkalmazotts = alkalmazott::paginate(5);
-        return view('dolgozo', compact('alkalmazott','alkalmazotts'));
+        
+        return view('dolgozo', 
+            ['alkalmazott' => alkalmazott::search('nev', $this->search)->paginate(5)]
+            ,compact('alkalmazott', 'alkalmazotts'));
     }
+
+    
+    /* public $sortField;
+    public $sortDirection='asc';
+
+    public function sortBy($field){
+        if($this->sortField===$field){
+            $this->sortDirection=$this->sortDirection==='asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortDirection='asc';
+        }
+        $this->sortField=$field;
+    }
+
+    public function rendez(){
+        return view('dolgozo', [
+            'alkalmazott' => alkalmazott::search('nev', $this->search)->orderBy($this->sortField, $this->sortDirection)->paginate(5),
+        ]);
+    } */
 }
