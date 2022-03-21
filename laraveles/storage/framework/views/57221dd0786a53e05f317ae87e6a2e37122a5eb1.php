1164611ae3bd6b695275b93d7942d5b8590c009e
KNikooo <kissnikolettanett@gmail.com>
2022-03-21 17:51:01 +0100

    agazati potlek modositas folytatas

--- laraveles/storage/framework/views/57221dd0786a53e05f317ae87e6a2e37122a5eb1.php
+++ laraveles/storage/framework/views/57221dd0786a53e05f317ae87e6a2e37122a5eb1.php
@@ -1,6 +1,7 @@
 
 <?php $__env->startSection('head'); ?>
     <link rel="stylesheet" href="css/agazatiPotlek.css" />
+    <script src="js/agazatiPotlek.js"></script>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('tartalom'); ?>
     <table class="table table-bordered mb-5">
@@ -20,25 +21,25 @@
                 <th scope="col">J</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="adatok">
             <?php $__currentLoopData = $agazatiPotlek; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $agazatiP): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <tr>
                     <td class="modositAgazatiP">
-			            <button class="btn btn-primary" id="modositadat" type="button" data-toggle="modal"
+			            <button class="btn btn-primary modositGomb"  id=<?php echo e($agazatiP->id); ?> type="button" data-toggle="modal"
 				            data-target="#exampleModalLong"><i class='fa fa-edit'></i>
 			            </button>
 		            </td>
-                    <td><?php echo e($agazatiP->eletkor); ?></td>
-                    <td><?php echo e($agazatiP->A); ?></td>
-                    <td><?php echo e($agazatiP->B); ?></td>
-                    <td><?php echo e($agazatiP->C); ?></td>
-                    <td><?php echo e($agazatiP->D); ?></td>
-                    <td><?php echo e($agazatiP->E); ?></td>
-                    <td><?php echo e($agazatiP->F); ?></td>
-                    <td><?php echo e($agazatiP->G); ?></td>
-                    <td><?php echo e($agazatiP->H); ?></td>
-                    <td><?php echo e($agazatiP->I); ?></td>
-                    <td><?php echo e($agazatiP->J); ?></td>
+                    <td class="eletkor" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->eletkor); ?></td>
+                    <td class="a" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->A); ?></td>
+                    <td class="b" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->B); ?></td>
+                    <td class="c" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->C); ?></td>
+                    <td class="d" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->D); ?></td>
+                    <td class="e" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->E); ?></td>
+                    <td class="f" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->F); ?></td>
+                    <td class="g" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->G); ?></td>
+                    <td class="h" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->H); ?></td>
+                    <td class="i" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->I); ?></td>
+                    <td class="j" id=<?php echo e($agazatiP->id); ?>><?php echo e($agazatiP->J); ?></td>
                 </tr>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </thead>
@@ -71,9 +72,9 @@
                                             <label class="form-label" for="agazatiPID">ID</label>
                                         </div>
                                     </div>
-                                    <div class="col">
+                                    <div class="col-7" style="transform: translateX(35%); text-align:center;">
                                         <div class="form-outline">
-                                            <input type="text" id="eletkor" class="form-control" readonly>
+                                            <input type="text" id="eletkor" class="form-control" nev="eletkor" readonly style="text-align:center;">
                                             <label class="form-label" for="eletkor">Életkor</label>
                                         </div>
                                     </div>
@@ -81,31 +82,31 @@
                                 <div class="row">
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="a" class="form-control" readonly>
+                                            <input type="text" id="a" class="form-control" name="a" readonly>
                                             <label class="form-label" for="a">A</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="b" class="form-control" readonly>
+                                            <input type="text" id="b" class="form-control" name="b" readonly>
                                             <label class="form-label" for="b">B</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="c" class="form-control" readonly>
+                                            <input type="text" id="c" class="form-control" name="c" readonly>
                                             <label class="form-label" for="c">C</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="d" class="form-control" readonly>
+                                            <input type="text" id="d" class="form-control" name="d" readonly>
                                             <label class="form-label" for="d">D</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="e" class="form-control" readonly>
+                                            <input type="text" id="e" class="form-control" name="e" readonly>
                                             <label class="form-label" for="e">E</label>
                                         </div>
                                     </div>
@@ -113,31 +114,31 @@
                                 <div class="row">
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="f" class="form-control" readonly>
+                                            <input type="text" id="f" class="form-control" name="f" readonly>
                                             <label class="form-label" for="f">F</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="g" class="form-control" readonly>
+                                            <input type="text" id="g" class="form-control" name="g" readonly>
                                             <label class="form-label" for="g">G</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="h" class="form-control" readonly>
+                                            <input type="text" id="h" class="form-control" name="h" readonly> 
                                             <label class="form-label" for="h">H</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="i" class="form-control" readonly>
+                                            <input type="text" id="i" class="form-control" name="i" readonly>
                                             <label class="form-label" for="i">I</label>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-outline">
-                                            <input type="text" id="j" class="form-control" readonly>
+                                            <input type="text" id="j" class="form-control" name="j" readonly>
                                             <label class="form-label" for="j">J</label>
                                         </div>
                                     </div>
